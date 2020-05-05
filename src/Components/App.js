23cccbe9b8163cee888e4b3c4532e3b79e6f45e3
Duong-Van-Bao <DuongBao9109@gmail.com>
2020-05-05 17:21:07 +0700

    ket noi du lieu

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react';
 import AppContainer from './appContainer/AppContainer';
 import Headers from './header/Headers';
 import Footer from './footer/Footer';
+import axios from 'axios';
 
 class App extends Component {
   render() {
+    axios.get('http://localhost:4000/getdata01')
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
     return (
         <div className="app">
           <Headers/>
